a67a68007e043206c9f93048e5592bde1cb370c0
Aleksandar Kostic <kostic@gmail.com>
2020-05-03 20:46:53 +0200

    Defined timenow var by importing time module, made title changable by using Jinja

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,20 +1,25 @@
+# from time import strftime, localtime
+import time
 from flask import render_template
 from program import app
+
+timenow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 
 @app.route("/")
 # @app.route("/index")
-def index():
+def root():
     # return render_template("index.html", title="Template Demo", time=timenow)
     return "Root!"
 
 
 @app.route("/index")
-def index2():
-    # return render_template("index.html", title="Template Demo", time=timenow)
-    return "Hello from index!"
+def index():
+    return render_template("index.html", title="Template Demo", time=timenow)
+    # return render_template("index.html", title="Template Demo")
+    # return "Hello from index!"
 
 
-# @app.route("/100Days")
-# def p100days():
-#     return render_template("100Days.html")
+@app.route("/100Days")
+def p100days():
+    return render_template("100Days.html", title="100 days page")
