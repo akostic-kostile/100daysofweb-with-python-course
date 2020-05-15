f5811ab6c690adfe5cba9ea2b48f9b3825d20602
Aleksandar Kostic <kostic@gmail.com>
2020-05-15 17:03:58 +0200

    Small tweaks and formating

--- days/017-020-flask-call-apis/code/program/routes.py
+++ days/017-020-flask-call-apis/code/program/routes.py
@@ -5,49 +5,46 @@ from flask import render_template, request
 
 from program import app
 
-time_now = str(datetime.today())
 
-
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html', title='Template Demo', time=time_now)
+    time_now = str(datetime.today())
+    return render_template("index.html", title="Template Demo", time=time_now)
 
 
-@app.route('/100Days')
+@app.route("/100Days")
 def p100days():
-    return render_template('100Days.html')
+    return render_template("100Days.html", title="100 days")
 
 
-@app.route('/chuck')
+@app.route("/chuck")
 def chuck():
     joke = get_chuck_joke()
-    return render_template('chuck.html',
-                           joke=joke)
+    return render_template("chuck.html", title="Chuck Jokes", joke=joke)
 
 
-@app.route('/pokemon', methods=['GET', 'POST'])
+@app.route("/pokemon", methods=["GET", "POST"])
 def pokemon():
     pokemon = []
-    if request.method == 'POST' and 'pokecolour' in request.form:
-        colour = request.form.get('pokecolour')
+    if request.method == "POST" and "pokecolour" in request.form:
+        colour = request.form.get("pokecolour")
         pokemon = get_poke_colours(colour)
-    return render_template('pokemon.html',
-                           pokemon=pokemon)
+    return render_template("pokemon.html", title="Pokemons", pokemon=pokemon)
 
 
 def get_chuck_joke():
-    r = requests.get('https://api.chucknorris.io/jokes/random')
+    r = requests.get("https://api.chucknorris.io/jokes/random")
     data = r.json()
-    return data['value']
+    return data["value"]
 
 
 def get_poke_colours(colour):
-    r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + colour.lower())
+    r = requests.get("https://pokeapi.co/api/v2/pokemon-color/" + colour.lower())
     pokedata = r.json()
     pokemon = []
 
-    for i in pokedata['pokemon_species']:
-        pokemon.append(i['name'])
+    for i in pokedata["pokemon_species"]:
+        pokemon.append(i["name"])
 
     return pokemon
