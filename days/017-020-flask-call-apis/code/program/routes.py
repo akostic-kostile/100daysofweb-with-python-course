cb696fd506fa463deaf36a6c0fbccf1af2a4ac06
Aleksandar Kostic <kostic@gmail.com>
2020-05-19 19:47:21 +0200

    Added basic error handling for pokemon and countries functions

--- days/017-020-flask-call-apis/code/program/routes.py
+++ days/017-020-flask-call-apis/code/program/routes.py
@@ -5,6 +5,19 @@ from flask import render_template, request
 
 from program import app
 
+VALID_POKE_COLOURS = [
+    "black",
+    "blue",
+    "brown",
+    "gray",
+    "green",
+    "pink",
+    "purple",
+    "red",
+    "white",
+    "yellow",
+]
+
 
 @app.route("/")
 @app.route("/index")
@@ -29,8 +42,29 @@ def pokemon():
     pokemon = []
     if request.method == "POST" and "pokecolour" in request.form:
         colour = request.form.get("pokecolour")
+        if colour.lower() not in VALID_POKE_COLOURS:
+            error_msg = f"Valid colours are {VALID_POKE_COLOURS}"
+            return render_template(
+                "pokemon.html", title="Pokemons", error_msg=error_msg
+            )
         pokemon = get_poke_colours(colour)
     return render_template("pokemon.html", title="Pokemons", pokemon=pokemon)
+
+
+@app.route("/country", methods=["GET", "POST"])
+def country():
+    countries = []
+    if request.method == "POST" and "country" in request.form:
+        user_input = request.form.get("country")
+        countries = get_country(user_input)
+        if isinstance(
+            countries, dict
+        ):  # searches that are not found return a dictionary rather than a list
+            error_msg = f"Not found '{user_input}', please repeat your serch"
+            return render_template(
+                "country.html", title="Countries", error_msg=error_msg
+            )
+    return render_template("country.html", title="Countries", countries=countries)
 
 
 def get_chuck_joke():
@@ -48,3 +82,9 @@ def get_poke_colours(colour):
         pokemon.append(i["name"])
 
     return pokemon
+
+
+def get_country(name):
+    r = requests.get(f"https://restcountries.eu/rest/v2/name/{name}")
+    data = r.json()
+    return data
